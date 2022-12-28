7f71b62fca5bab630edfc3f3509193942604e80c
Sara Raley <sara.m.raley@gmail.com>
2022-12-28 13:26:00 -0700

    creates education model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_28_191538) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_28_201852) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_28_191538) do
     t.text "capstone_description"
     t.string "capstone_url"
     t.string "capstone_screenshot"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "educations", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.string "degree"
+    t.string "university_name"
+    t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
