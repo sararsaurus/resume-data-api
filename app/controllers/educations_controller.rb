f392307394aae25feae680033b5dd74c7e210592
Sara Raley <sara.m.raley@gmail.com>
2022-12-28 14:02:53 -0700

    adds education routes

---
+++ app/controllers/educations_controller.rb
@@ -0,0 +1,16 @@
+class EducationsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  ### EDUCATION
+  get "/educations" => "educations#index"
+  get "/educations/:id" => "educations#show"
+  post "/educations" => "educations#create"
+  patch "/educations/:id" => "educations#update"
+  delete "/educations" => "educations#destroy"
 end
