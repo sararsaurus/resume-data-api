9df5d6b85511bd8b6b9e81e33592eb1e38f0bfc6
Sara Raley <sara.m.raley@gmail.com>
2022-12-28 13:32:32 -0700

    adds student_id to educations table

---
+++ db/migrate/20221228202930_add_student_id_to_educations.rb
@@ -0,0 +1,5 @@
+class AddStudentIdToEducations < ActiveRecord::Migration[7.0]
+  def change
+    add_column :educations, :student_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_28_201852) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_28_202930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_28_201852) do
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "student_id"
   end
 
   create_table "experiences", force: :cascade do |t|
