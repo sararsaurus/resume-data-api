c6c6c432b37d3a6fcb5aa15aee075421a75733a4
StevenMetz <stevenm.codes@gmail.com>
2022-12-28 17:14:56 -0600

    update works

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -32,6 +32,24 @@ class StudentsController < ApplicationController
   end
 
   def update
+    student = Student.find_by(id: params["id"])
+    student.first_name = params["first_name"] || student.first_name
+    student.last_name = params["last_name"] || student.last_name
+    student.email = params["email"] || student.email
+    student.phone_number = params["phone_number"] || student.phone_number
+    student.short_bio = params["short_bio"] || student.short_bio
+    student.linkedin_url = params["linkedin_url"] || student.linkedin_url
+    student.twitter_handle = params["twitter_handle"] || student.twitter_handle
+    student.online_resume_url = params["online_resume_url"] || student.online_resume_url
+    student.github_url = params["github_url"] || student.github_url
+    student.photo_url = params["photo_url"] || student.photo_url
+    student.personal_website_url = params["personal_website_url"] || student.personal_website_url
+    @student = student
+    if student.save
+      render json: student
+    else
+      render json: { errors: student.errors.full_messages }, status: 418
+    end
   end
 
   def destroy
