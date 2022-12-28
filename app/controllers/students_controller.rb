10d3e4e2a6b9a5e856654bc8b2f785f7cae81b19
StevenMetz <stevenm.codes@gmail.com>
2022-12-28 15:05:31 -0600

    create has been created need to add error messages

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,27 @@
 class StudentsController < ApplicationController
   def index
     students = Student.all
-    render json: students.as_jsong
+    render json: students.as_json
   end
 
   def create
+    @students = Student.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      short_bio: params[:short_bio],
+      linkedin_url: params[:linkedin_url],
+      twitter_handle: params[:twitter_handle],
+      online_resume_url: params[:online_resume_url],
+      github_url: params[:github_url],
+      photo_url: params[:photo_url],
+      personal_website_url: params[:personal_website_url],
+
+    )
+    @students.save
+    if @students.save
+    end
   end
 
   def show
