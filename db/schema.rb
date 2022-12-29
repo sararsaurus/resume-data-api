1609c25915dd668c450018b8465b0fa90ce9fdc3
Connor La Rocca <connorlarocca@hotmail.com>
2022-12-29 12:59:24 -0500

    added student id to experiences and capstones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_28_231617) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_29_175815) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_28_231617) do
     t.string "capstone_screenshot"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "student_id"
   end
 
   create_table "educations", force: :cascade do |t|
@@ -42,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_28_231617) do
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "student_id"
   end
 
   create_table "skills", force: :cascade do |t|
