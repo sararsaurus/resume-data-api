73020f46dc07cfb3a8fcb76ca1765f663001a260
StevenMetz <stevenm.codes@gmail.com>
2022-12-29 12:06:56 -0600

    migrated stevens database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_29_174309) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_29_175815) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_29_174309) do
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "student_id"
   end
 
   create_table "skills", force: :cascade do |t|
@@ -79,4 +78,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_29_174309) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
   end
+
 end
