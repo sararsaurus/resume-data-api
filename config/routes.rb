2c6e25cff21383992201a788755083d5d20a1225
hannahekehoe <hkehoe90@gmail.com>
2022-12-29 13:25:15 -0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   resources :students
-
-  ### EDUCATION
-  get "/educations" => "educations#index"
-  get "/educations/:id" => "educations#show"
-  post "/educations" => "educations#create"
-  patch "/educations/:id" => "educations#update"
-  delete "/educations/:id" => "educations#destroy"
+  resources :educations
+  resources :capstones
+  resources :experiences
+  resources :skills
 
   ### SESSIONS
   post "/sessions" => "sessions#create"
