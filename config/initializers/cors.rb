aff6a05a6e00b6d030fc5707ac08698bfb7f7738
StevenMetz <stevenm.codes@gmail.com>
2022-12-30 13:19:46 -0600

    cors regex corrected

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "/localhost:\d+/"
+    origins /localhost:\d+/
 
     resource "*",
       headers: :any,
